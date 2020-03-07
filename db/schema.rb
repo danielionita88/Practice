0f6eca3f33fef0bf01bf10508520788adfa816ad
Daniel Ionita <danielionita88@yahoo.com>
2020-03-07 14:14:54 -0600

    create and delete comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_07_175456) do
+ActiveRecord::Schema.define(version: 2020_03_07_192401) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
